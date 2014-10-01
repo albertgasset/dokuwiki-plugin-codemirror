bc1c32169fae7612761a05868b1c4384daef7883
SC Yoo <dryoo@live.com>
2014-10-01 12:21:22 +0900

    korean lang.php

---
+++ lang/ko/lang.php
@@ -0,0 +1,10 @@
+<?php 
+$lang['js']['setting_activeline'] = '현재 행 강조 표시'; 
+$lang['js']['setting_closebrackets'] = '괄호 자동 입력'; 
+$lang['js']['setting_fontsize'] = '글꼴 크기'; 
+$lang['js']['setting_keymap'] = '키보드 매핑'; 
+$lang['js']['setting_linenumbers'] = '행 번호 표시'; 
+$lang['js']['setting_matchbrackets'] = '해당 괄호 강조'; 
+$lang['js']['setting_syntax'] = '구문 강조'; 
+$lang['js']['setting_theme'] = '색상 테마';
+
