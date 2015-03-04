214d77a48d7bf6a1b79d5e9b8cb21d9df4d8916e
konus77 <kontakt@dd4kids.de>
2015-03-05 00:11:31 +0100

    add german langfile

---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['js']['setting_activeline'] = 'Aktuelle Zeile hervorheben';
+$lang['js']['setting_closebrackets'] = 'Klammern automatisch schließen';
+$lang['js']['setting_fontsize'] = 'Font Größe';
+$lang['js']['setting_keymap'] = 'Tastenzuordnung';
+$lang['js']['setting_linenumbers'] = 'Zeige Zeilennummern';
+$lang['js']['setting_matchbrackets'] = 'passende Klammern hervorheben';
+$lang['js']['setting_syntax'] = 'Syntax hervorheben';
+$lang['js']['setting_theme'] = 'Farbschema';
