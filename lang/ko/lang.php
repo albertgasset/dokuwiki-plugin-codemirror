ae90a4ca76a6f9235d85eefdefe72d238c48b669
Myeongjin <aranet100@gmail.com>
2015-08-02 21:49:45 +0900

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,10 +1,11 @@
-<?php 
-$lang['js']['setting_activeline'] = '현재 행 강조 표시'; 
-$lang['js']['setting_closebrackets'] = '괄호 자동 입력'; 
-$lang['js']['setting_fontsize'] = '글꼴 크기'; 
-$lang['js']['setting_keymap'] = '키보드 매핑'; 
-$lang['js']['setting_linenumbers'] = '행 번호 표시'; 
-$lang['js']['setting_matchbrackets'] = '해당 괄호 강조'; 
-$lang['js']['setting_syntax'] = '구문 강조'; 
-$lang['js']['setting_theme'] = '색상 테마';
+<?php
 
+$lang['js']['setting_activeline'] = '현재 줄 강조';
+$lang['js']['setting_closebrackets'] = '괄호 자동 닫기';
+$lang['js']['setting_fontsize'] = '글꼴 크기';
+$lang['js']['setting_keymap'] = '키보드 매핑';
+$lang['js']['setting_linenumbers'] = '줄 번호 보이기';
+$lang['js']['setting_matchbrackets'] = '일치하는 괄호 강조';
+$lang['js']['setting_nativeeditor'] = '네이티브 도쿠위키 편집기';
+$lang['js']['setting_syntax'] = '구문 강조';
+$lang['js']['setting_theme'] = '색상 테마';
