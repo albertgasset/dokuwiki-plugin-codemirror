a0037094051b735c29ae3e3a39cfd05cac298ebc
Fabrice Dejaigher <fabrice@chtiland.com>
2016-01-05 10:00:38 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Fabrice Dejaigher <fabrice@chtiland.com>
+ */
+$lang['nativeeditor']          = 'Utiliser l\'éditeur natif de Dokuwiki par défaut';
