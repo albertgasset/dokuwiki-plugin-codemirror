a8c75029d044204d9a25a3664ad5d20fb5440d5e
dryoo <dryoo@live.com>
2016-06-27 20:02:11 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -1,3 +1,9 @@
 <?php
 
-$lang['nativeeditor'] = '네이티브 도쿠위키 편집기를 기본값으로 사용';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author dryoo <dryoo@live.com>
+ */
+$lang['nativeeditor']          = '네이티브 도쿠위키 편집기를 기본값으로 사용';
+$lang['codesyntax']            = '페이지를 볼 때 코드블럭의 문법강조를 위해 코드미러를 사용함.';
