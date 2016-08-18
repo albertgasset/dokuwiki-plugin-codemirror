a6fd3bef5644eefb37b31802094e36398688f7d6
Marcel Bachus <marcel.bachus@ziggo.nl>
2016-08-18 12:00:04 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marcel Bachus <marcel.bachus@ziggo.nl>
+ */
+$lang['nativeeditor']          = 'ebruik de eigen DokuWiki editor als standaard.';
+$lang['codesyntax']            = 'Gebruik CodeMirror om de syntaxes of de code blokken te accentueren wanneer de pagina wordt bekeken.';
