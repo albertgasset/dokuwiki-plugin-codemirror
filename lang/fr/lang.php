fbd55c945e6e1836a8e4b135cd458c161f7bf9f3
Digitalin <digikatya@yahoo.fr>
2016-08-28 11:44:00 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['js']['setting_activeline'] = 'Surligner la ligne courante';
+$lang['js']['setting_closebrackets'] = 'Parenthèses auto-fermantes';
+$lang['js']['setting_fontsize'] = ' Taille de police';
+$lang['js']['setting_keymap'] = 'Éditeur - raccourcis';
+$lang['js']['setting_linenumbers'] = 'Afficher les numéros de lignes';
+$lang['js']['setting_matchbrackets'] = 'Surligner la correspondance des parenthèses';
+$lang['js']['setting_nativeeditor'] = 'Éditeur natif DokuWiki';
+$lang['js']['setting_syntax'] = 'Surligner la syntaxe';
+$lang['js']['setting_theme'] = 'Couleur du thème';
