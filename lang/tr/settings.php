9a174e20dc31e2fdd09867f435b3d12514652d44
İlker R. Kapaç <irifat@gmail.com>
2017-01-13 08:00:05 +0100

    translation update

---
+++ lang/tr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author İlker R. Kapaç <irifat@gmail.com>
+ */
+$lang['nativeeditor']          = 'DokuWiki\'nin dahili düzenleyicisini kullan.';
+$lang['codesyntax']            = 'Sayfa görüntülenirken söz dizimindeki kod parçalarını CodeMirror ile vurgula.';
