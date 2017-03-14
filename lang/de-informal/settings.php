d0845be119f7491d3c72ef73cca3576b77689396
Felix Mueller-Donath <j.felix@mueller-donath.de>
2017-03-14 15:16:48 +0100

    Create settings.php

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Martin Sachers <martin.sachers@etron.at>
+ * @author Felix Mueller-Donath <j.felix@mueller-donath.de>
+ */
+ 
+$lang['nativeeditor']          = 'Nativen DokuWiki Editor als Standard verwenden';
+$lang['codesyntax']            = 'Verwende CodeMirror um die Syntax hervorzuheben wenn du Seiten betrachtest.';
