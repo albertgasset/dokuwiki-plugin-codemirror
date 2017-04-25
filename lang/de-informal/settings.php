81f4bc13fdd94fd38eef4b585ca74d3a84fcd525
F. Mueller-Donath <j.felix@mueller-donath.de>
2017-04-25 09:20:27 +0200

    translation update

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['nativeeditor']          = 'Verwende den nativen DokuWiki Editor als Standard';
+$lang['codesyntax']            = 'Verwenden CodeMirror um die Syntax hervorzuheben wenn du Seiten betrachtest.';
+$lang['usenativescroll']       = 'Verwende den nativen Scrollbar-Stil statt des Standard Overlay Stils';
